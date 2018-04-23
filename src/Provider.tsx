143f3b2d783a6c8b2cf313980cf1dd1e27b3af51
aexol <aexol@aexol.com>
2018-04-23 13:43:49 +0200

    syncanocontainertype

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -2,18 +2,22 @@ import * as React from 'react';
 import { Provider, ProviderProps } from 'unstated';
 import { Auth, Rest, SyncanoContainer } from './containers';
 import * as SyncanoClient from '@syncano/client';
+export type SyncanoContainerType = {
+  new (s: any): SyncanoContainer<any>;
+};
 export type SyncanoProviderProps = ProviderProps & {
   instanceName: string;
   syncanoContainers?: {
-    [x: string]: SyncanoContainer<any>;
+    [x: string]: SyncanoContainerType;
   };
 };
+
 export const SyncanoProvider = ({
   instanceName,
   syncanoContainers = {},
   ...props
 }: SyncanoProviderProps) => {
-  let injected = { Auth, Rest, ...syncanoContainers };
+  let injected: { [x: string]: SyncanoContainerType } = { Auth, Rest, ...syncanoContainers };
   let inj = [];
   let sync = SyncanoClient(instanceName);
   for (var k of Object.keys(injected)) {
